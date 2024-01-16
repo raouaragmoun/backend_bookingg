8a48a5710e85e307ffbeb1149e63de0f7482210b
raouaragmoun <raouaragmoun88@gmail.com>
2024-01-16 15:25:31 +0100

    Premier commit

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-import  Express  from "express";
-import { login, register } from "../controller/authController.js";
-const router = Express.Router();
-
-router.post("/login", login);
-
-router.post("/register", register);
-
-export default router;
